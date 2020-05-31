19eca5fb31302b8257343a3d2b517f58940c7451
uniquelise01 <han19008@byui.edu>
2020-05-31 00:05:23 -0600

    finished w06 js

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -1,12 +1,12 @@
 //Wind chill calculation
-let t = parseFloat(document.getElementById("temp").value);
-let s = parseFloat(document.getElementById("windspeed").value);
+let t = parseFloat(document.getElementById("temp").innerText);
+let s = parseFloat(document.getElementById("windspeed").innerText);
 let wchill = 35.74 + (0.6215 * t) - (35.75 * (s**0.16)) + (0.4275 * t * (s**0.16));
 
-document.getElementById("windchill").innerHTML = parseInt(wchill);
-
-if (wchill === NaN) {
+if (isNaN(wchill)) {
     document.getElementById("windchill").innerHTML = "N/A";
 }
 
-console.log(t);
+else {
+    document.getElementById("windchill").innerHTML = parseInt(wchill);
+}
