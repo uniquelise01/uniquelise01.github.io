abc3a8b18464ec2d8233c039fc34c45abd1172cb
uniquelise01 <han19008@byui.edu>
2020-06-05 22:44:38 -0600

    java fun

--- lesson7/js/preston-7.js
+++ lesson7/js/preston-7.js
@@ -10,6 +10,20 @@ let monthname = m_names[d.getMonth()];
 
 mydate.textContent = weekname + ", " + d.getDate() + " " + monthname + " " + d.getFullYear();
 
+//5 Day Forecast
+let abbrw_names = ['Sun', 'Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat', 'Sun', 'Mon', 'Tue', 'Wed', 'Thur'];
+let dayone = abbrw_names[d.getDay()+1];
+let daytwo = abbrw_names[d.getDay()+2];
+let daythree = abbrw_names[d.getDay()+3];
+let dayfour = abbrw_names[d.getDay()+4];
+let dayfive = abbrw_names[d.getDay()+5];
+console.log(dayfive);
+
+document.getElementById("day1").innerHTML = dayone;
+document.getElementById("day2").innerHTML = daytwo;
+document.getElementById("day3").innerHTML = daythree;
+document.getElementById("day4").innerHTML = dayfour;
+document.getElementById("day5").innerHTML = dayfive;
 
 //Hamburger menu
 const mainnav = document.querySelector('.navigation');
